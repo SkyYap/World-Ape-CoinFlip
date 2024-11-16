c420345a6de92fab87b31e0f5b259e8a5d06196a
SkyYap <32660903+SkyYap@users.noreply.github.com>
2024-11-16 21:14:50 +0800

    chnage some frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { Flex } from '@chakra-ui/react';
 export default function App() {
   return (
     <main 
-      className="flex min-h-screen flex-col items-center justify-center gap-y-3" 
+      className="flex min-h-screen flex-col items-center justify-center gap-y-3 overflow-x-hidden" 
       style={{ backgroundColor: '#076A69' }}
     >
       <Flex 
